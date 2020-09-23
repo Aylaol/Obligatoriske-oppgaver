a7fa61f9ad5a9cfa8dddd0e345d6d6bb8488644c
Aylaol <ayla@getacademy.no>
2020-09-23 11:02:36 +0200

    yaaah

---
+++ Oblig2/oblig2.js
@@ -0,0 +1,25 @@
+function stolpeValgt(barNo) {
+    chosenBar = barNo == chosenBar ? null : barNo;
+    show();
+}
+
+function removeChoice() {
+    numbers.splice(chosenBar - 1, 1);
+    show();
+}
+
+function changeChoice() {
+    numbers.splice(chosenBar - 1, 1, inputValue);
+    if (inputValue > 10) {
+        return alert("Stolpe verdien kan kun være 10 eller lavere");
+    }
+    show();
+}
+
+function addChoice() {
+    numbers.splice(chosenBar + 0, 0, inputValue);
+    if (inputValue > 10) {
+        return alert("Stolpe verdien kan kun være 10 eller lavere");
+    }
+    show();
+}
